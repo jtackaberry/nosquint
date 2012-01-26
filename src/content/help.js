ccbd76eff10c3c9499e637528d62cf94c17b06aa
Jason Tackaberry <tack@urandom.ca>
2012-01-25 20:22:07 -0500

    Remove unused files that accidentally slipped in from the dev branch

--- src/content/help.js
+++
@@ -1,9 +0,0 @@
-var NoSquintHelp = {
-
-    init: function(doc) {
-        var b = doc.getElementById("nosquint-help-browser");
-        //b.webBrowserFind.searchString = "NoSquint";
-        //b.webBrowserFind.findNext();
-    }
-};
-
